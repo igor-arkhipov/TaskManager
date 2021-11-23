bbb23a1cee3a8216dfd3994b78bf3090042cab4f
Igor Arkhipov <igor.arkhipov@outlook.com>
2021-11-23 16:46:06 +0300

    implement finite automaton

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,39 +1,38 @@
 class Task < ApplicationRecord
-	belongs_to :author, class_name: 'User'
-	belongs_to :assignee, class_name: 'User', optional: true
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
-	validates :name, :description, :author, presence: true
-	validates :description, length: { maximum: 500 }
-	
-	state_machine initial: => :new_task do
-		event :to_dev do
-			transition new_task: => :in_development
-		end
-	
-		event :back_to_dev do
-			transition in_qa: => :in_development
-			transition in_code_review: => :in_development
-		end
-	
-		event :to_qa do
-			transition new_task: => :in_qa
-		end
-	
-		event :to_code_reveiw do
-			transition in_qa: => :in_code_review
-		end
-	
-		event :pre_release do
-			transition in_code_review: => :ready_for_release
-		end
-	
-		event :release do
-			transition ready_for_release: => :released
-		end
-	
-		event :archive do
-			transition new_task: => :archived
-			transition released: => :archived
-		end
-	end
+  validates :name, :description, :author, presence: true
+  validates :description, length: { maximum: 500 }
+
+  # [TODO] Look later why test/models/task_test.rb fails
+  state_machine initial: :new_task do
+    event :to_dev do
+      transition new_task: :in_development
+    end
+
+    event :back_to_dev do
+      transition from: [:in_qa, :in_code_review], to: :in_development
+    end
+
+    event :to_qa do
+      transition new_task: :in_qa
+    end
+
+    event :to_code_reveiw do
+      transition in_qa: :in_code_review
+    end
+
+    event :pre_release do
+      transition in_code_review: :ready_for_release
+    end
+
+    event :release do
+      transition ready_for_release: :released
+    end
+
+    event :archive do
+      transition from: [:new_task, :released], to: :archived
+    end
+  end
 end
