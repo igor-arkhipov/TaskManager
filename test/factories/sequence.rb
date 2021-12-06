c46557ea71679aa95389c8f4623753312772814d
Igor Arkhipov <igor.arkhipov@outlook.com>
2021-12-06 10:07:11 +0300

    refactor some fabrics

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :avatar] do |n|
+  sequence :string, aliases: [:password, :avatar] do |n|
     "string#{n}"
   end
+
+  sequence :first_name, %i[Ivan Piotr Sergey Sanya Kirill].cycle
+
+  sequence :last_name, %i[Ivanov Piotrov Sergeev Kuznetcov].cycle
 
   sequence :email do |n|
     "stranger#{n}@example.xyz"
