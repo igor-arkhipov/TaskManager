2f9799a761555a36d89d4ca7337456aba600d515
Igor Arkhipov <igor.arkhipov@outlook.com>
2021-12-08 03:15:32 +0300

    fix controller

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -1,13 +1,14 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionController::TestCase
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+
   test 'should get new' do
     get :show
     assert_response :success
   end
 
-  setup do
-    user = create(:user)
-    sign_in user
-  end
 end
