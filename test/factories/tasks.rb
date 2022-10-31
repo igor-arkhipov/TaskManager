1791ae378323bb2b9f2440e928d47f9d780ef0ed
Igor Arkhipov <igor.arkhipov@outlook.com>
2022-11-01 02:13:54 +0300

    after-test task controller and factory test

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description { 'MyText' }
     author factory: :manager
     assignee factory: :developer
-    state { :new_task }
-    expired_at { '2021-11-19' }
+    state { 'new_task' }
+    # expired_at { '2021-11-19' }
   end
 end
